fa141709f1ae95ae977328666ad7b3f605f3590c
BadPixxel <eshop.bpaquier@gmail.com>
2026-02-27 13:56:30 +0100

    UPDATE - Refactor Brevo connector to implement OpenAPI, enhance connection handling, and replace legacy attribute and list fetching logic

--- src/DependencyInjection/BrevoExtension.php
+++ src/DependencyInjection/BrevoExtension.php
@@ -15,15 +15,17 @@
 
 namespace Splash\Connectors\Brevo\DependencyInjection;
 
+use Splash\Connectors\Brevo\Services\Connexion\BrevoRateLimiter;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * Loads and manages bundle configuration
  */
-class BrevoExtension extends Extension
+class BrevoExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -34,5 +36,23 @@ class BrevoExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container): void
+    {
+        //==============================================================================
+        // Configure Framework Rate Limiter
+        $container->prependExtensionConfig('framework', array(
+            'rate_limiter' => array(
+                BrevoRateLimiter::CONFIG_KEY => array(
+                    'policy' => 'sliding_window',
+                    'limit' => 20,
+                    'interval' => '1 second',
+                ),
+            ),
+        ));
     }
 }
